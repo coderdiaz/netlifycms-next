1ba2da3bb81482afb2cb7293430bd984097f58ee
Javier Diaz <hello@coderdiaz.me>
2019-05-26 22:22:51 -0500

    feat: fetching data from api in Index Page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,31 @@
 import withLayout from '../components/Layout.js'
+import Link from 'next/link'
+import fetch from 'isomorphic-unfetch'
 
-const Index = () => <p>Hello Next.js</p>
+const Index = props => (
+  <div>
+    <h1>Batman TV Shows</h1>
+    <ul>
+      {props.shows.map(show => (
+        <li key={show.id}>
+          <Link as={`p/${show.id}`} href={`/post?id=${show.id}`}>
+            <a>{show.name}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </div>
+)
+
+Index.getInitialProps = async () => {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman')
+  const data = await res.json()
+
+  console.log(`Show data fetched. Count: ${data.length}`)
+
+  return {
+    shows: data.map(entry => entry.show)
+  }
+}
 
 export default withLayout(Index);
