5a429718e0eea7a2c22d19c9534a7be848e93db8
Javier Diaz <hello@coderdiaz.me>
2019-05-26 22:31:19 -0500

    fix: fixed blog page

--- pages/blog.js
+++ pages/blog.js
@@ -1,22 +1,10 @@
 import withLayout from '../components/Layout'
 import Link from 'next/link'
 
-const PostLink = props => (
-  <li>
-    <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
-      <a>{props.title}</a>
-    </Link>
-  </li>
-)
-
 const Blog = () => (
   <div>
     <h1>My Blog</h1>
-    <ul>
-      <PostLink id="hello-nextjs" title="Hello Next.js" />
-      <PostLink id="learn-nextjs" title="Learn Next.js is awesome" />
-      <PostLink id="deploy-nextjs" title="Deploy apps with Zeit" />
-    </ul>
+    <p>Space for show entries</p>
   </div>
 )
 
